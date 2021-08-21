1ce0c5606661a56b4fc456b529dd955da37f0d95
daniel.wang04 <daniel.wang04@hotmail.com>
2021-08-21 16:15:36 -0400

    update

--- entry screen.py
+++ entry screen.py
@@ -10,27 +10,26 @@ title = "Create a new record"
 
 class Button(arcade.gui.UIFlatButton):
     def on_click(self):
-        print("Clicked")
-
+        pass
 
 class GeneratePasswordButton(arcade.gui.UIFlatButton):
     def on_click(self):
-        print("Clicked")
+        pass
 
 
 class EntryScreen(arcade.View):
 
     def __init__(self):
         super().__init__()
-        arcade.set_background_color(arcade.color.LIGHT_STEEL_BLUE)
+        arcade.set_background_color(arcade.color.BLACK)
         self.ui_manager = UIManager()
 
     def on_draw(self):
         arcade.start_render()
-        arcade.draw_text("Application:", start_x=20, start_y=600, color=arcade.color.BLACK, font_size=20)
-        arcade.draw_text("URL:", start_x=20, start_y=500, color=arcade.color.BLACK, font_size=20)
-        arcade.draw_text("Username:", start_x=20, start_y=400, color=arcade.color.BLACK, font_size=20)
-        arcade.draw_text("Password:", start_x=20, start_y=300, color=arcade.color.BLACK, font_size=20)
+        arcade.draw_text("Nickname:", start_x=20, start_y=600, color=arcade.color.WHITE, font_size=20)
+        arcade.draw_text("URL:", start_x=20, start_y=500, color=arcade.color.WHITE, font_size=20)
+        arcade.draw_text("Username:", start_x=20, start_y=400, color=arcade.color.WHITE, font_size=20)
+        arcade.draw_text("Password:", start_x=20, start_y=300, color=arcade.color.WHITE, font_size=20)
 
     def on_show_view(self):
         self.setup()
@@ -44,8 +43,8 @@ class EntryScreen(arcade.View):
         # add button
         button = Button("Create entry", center_x=200, center_y=200, width=250)
         self.ui_manager.add_ui_element(button)
-        # input box application
-        input_box = arcade.gui.UIInputBox(center_x=270, center_y=620, width=245)
+        # input box nickname
+        input_box = arcade.gui.UIInputBox(center_x=260, center_y=620, width=255)
         input_box.text = ''
         input_box.cursor_index = len(input_box.text)
         self.ui_manager.add_ui_element(input_box)
